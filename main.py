f998a5f6342c67cd0250b4ceeadc78b766381dd2
Jason JEONG <lima36@empal.com>
2021-11-25 19:34:50 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from jauro import *
+from kucc import *
+
+if __name__ == "__main__":
+    # cc = jauro()
+    cc = kucc()
+    cc.Start()
+    cc.Login()
+    cc.ClosePopup()
+    cc.Reserve()
+    
