0f7c254aea0e857c9fc808248bf34702b2d09c22
Jason JEONG <lima36@empal.com>
2021-11-25 19:35:09 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+import datetime
+from datetime import date
+
+
+def get_today():
+    return date.today()
+
+def get_target(days):
+    target = datetime.datetime.now() + datetime.timedelta(days=days)
+    return target
